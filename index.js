0d841d5cff0a3142fefa1e25f0c0a6381cad4e85
whiplade <nmawira13@gmail.com>
2023-06-18 14:31:12 +0300

    Assignment Complete

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"];
+const event = 'surprise'
+let birthdayCard = [];
+
+function writeCards(names, event) {
+
+  for (let i = 0; i < names.length; i++) {
+
+    let note = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    birthdayCard.push(note);
+  }
+  console.log(birthdayCard);
+  return birthdayCard;
+}
+
+console.log(writeCards(names, events));
+
+function countDown(number){
+    let countDown = 10;
+    while(countDown >= -0){
+        console.log(countDown --);
+    }
+}
+console.log(countDown(10));
